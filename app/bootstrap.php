770913f69ca10101b87397d361b59ae919eea3fb
John Dinh <johnduydinh@gmail.com>
2020-03-31 12:41:26 -0400

    Initial setup for framework
    Mainly setting up Core.php to handle the framework's URL structure of
    controller/method/params

---
+++ app/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+// Enable errors
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+// Load Libraries
+require_once('libraries/Core.php');
+require_once('libraries/Controller.php');
+require_once('libraries/Database.php');
+
+?>
