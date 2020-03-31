8404124758e5ae174b6fd092f9aa5058e9dc74f5
John Dinh <johnduydinh@gmail.com>
2020-03-31 13:28:08 -0400

    Autoload core libraries & create config file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,9 +5,12 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
-// Load Libraries
-require_once('libraries/Core.php');
-require_once('libraries/Controller.php');
-require_once('libraries/Database.php');
+// Load Config
+require_once('config/config.php');
+
+// Autoload Core Libraries
+spl_autoload_register(function($className) {
+    require_once 'libraries/' . $className . '.php';
+});
 
 ?>
